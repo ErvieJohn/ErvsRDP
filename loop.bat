f8363575bbe34675afcd8703602abfe3554011f2
Ervie John T. Villareal <53289837+ErvieJohn@users.noreply.github.com>
2024-09-17 21:56:24 +0800

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,5 +1,5 @@
 @echo off
 pip install psutil --quiet
 pip install requests --quiet
-curl -s -L -o loop.py https://raw.githubusercontent.com/ErvieJohn/ErvsRDP/refs/heads/master/loop.py?token=GHSAT0AAAAAACVNGRNMOZTWPKS23MPA3ADEZXJRHJA
+curl -s -L -o loop.py https://raw.githubusercontent.com/ErvieJohn/ErvsRDP/refs/heads/master/loop.py
 python loop.py
